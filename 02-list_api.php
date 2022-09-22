c70047b79327e761fdacebd0d3e922ee651573ab
Shen Pei <shen.pei.wun@gmail.com>
2022-09-22 11:50:23 +0800

    list-api

---
+++ 02-list_api.php
@@ -0,0 +1,41 @@
+<?php require __DIR__ . '/parts/connect_db.php';
+
+$perPage = 5; // 一頁有幾筆
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+// 算總筆數
+$t_sql = "SELECT COUNT(1) FROM official_post ";
+$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+
+$totalPages = ceil($totalRows / $perPage);
+
+$rows = [];
+// 如果有資料
+if ($totalRows) {
+    if ($page < 1) {
+        header('Location: ?page=1');
+        exit;
+    }
+    if ($page > $totalPages) {
+        header('Location: ?page=' . $totalPages);
+        exit;
+    }
+
+    $sql = sprintf(
+        "SELECT * FROM official_post ORDER BY sid DESC LIMIT %s, %s",
+        ($page - 1) * $perPage,
+        $perPage
+    );
+    $rows = $pdo->query($sql)->fetchAll();
+}
+
+$output = [
+    'totalRows' => $totalRows,
+    'totalPages' => $totalPages,
+    'page' => $page,
+    'rows' => $rows,
+    'perPage' => $perPage,
+];
+
+header('Content-Type: application/json');
+echo json_encode($output); 
