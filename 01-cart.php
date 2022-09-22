7fdb29c8e5bbb1fc6f9e30387700eb9abf8ec90a
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-22 18:29:53 +0800

    add remove/update

--- 01-cart.php
+++ 01-cart.php
@@ -1,6 +1,7 @@
 <?php
 include __DIR__ . '/parts/connect_db.php';
 $pageName = 'cart';
+
 ?>
 <?php
 include __DIR__ . '/parts/html-head.php'; ?>
@@ -23,28 +24,29 @@ include __DIR__ . '/parts/nav-bar-no-admin.php'; ?>
                 <table class="table table-striped table-bordered cart-table">
                     <thead>
                         <tr>
-                            <th scope="col">項次</th>
+                            <th scope="col">#</th>
                             <th scope="col">商品名稱</th>
                             <th scope="col">價格</th>
                             <th scope="col">數量</th>
                             <th scope="col">小計</th>
                             <th scope="col">
-                                刪除商品</i>
+                                <i class="fa-solid fa-trash"></i>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($_SESSION['cart'] as $c => $v) :
-                            $total += $v['price'] * $v['qty'];
+                        <?php
+                        $total = 0;
+                        foreach ($_SESSION['cart'] as $k => $v) :
+                            $total += $v['prce'] * $v['qty'];
                         ?>
                             <tr data-sid="<?= $k ?>" class="cart-item">
                                 <td><?= $k ?></td>
                                 <td><?= $k['product_name'] ?></td>
                                 <td>
-                                    <select class="w-75 form-select">
-                                        <option selected>選擇數量</option>
+                                    <select class="w-75 form-select qty" onchange="updateItem(event)">
                                         <?php for ($i = 1; $i <= 5; $i++) : ?>
-                                            <option value="<?= $i ?>"><?= $i ?></option>
+                                            <option value="<?= $i == $V['qty'] ? 'selected' : '' ?>"><?= $i ?></option>
                                         <? endfor; ?>
                                     </select>
                                 </td>
@@ -63,18 +65,77 @@ include __DIR__ . '/parts/nav-bar-no-admin.php'; ?>
         <div class="alert alert-success" role="alert">
             <span>總計：</span id="total-price"><span></span>元
         </div>
-        <div>
-            <?php if (empty($_SESSION['user'])) : ?>
-                <div class="alert alert-danger" role="alert">
-                    請先登入會員, 再結帳
-                </div>
-            <?php else : ?>
-
-            <?php endif; ?>
-        </div>
+        <?php if (empty($_SESSION['user'])) : ?>
+            <div class="alert alert-danger" role="alert">
+                請先登入會員, 再結帳
+            </div>
+        <?php else : ?>
+            <a href="buy.php" class="btn btn-warning">結帳</a>
+        <?php endif; ?>
     <? endif; ?>
 </div>
 <?php
 include __DIR__ . '/parts/scripts.php'; ?>
+<script>
+    const dollorCommas = function(n) {
+        return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    };
+
+    function removeItem(event){
+        const tr = $(event.currentTarget).closest('tr');
+        const sid = tr.attr('data-sid');
+
+        $.get(
+            '01-handle-cart.php', {
+                sid
+            },
+            function(data) {
+                console.log(data);
+                showCartCount(data);
+                tr.remove();
+
+                updatePrices();
+            },
+            'json');
+    }
+
+    function updateItem(event) {
+        const sid = $(event.currentTarget).closest('tr').attr('data-sid');
+        const qty = $(event.currentTarget).val();
+
+        $.get(
+            '01-handle-cart.php', {
+                sid,
+                qty
+            },
+            function(data) {
+                console.log(data);
+                showCartCount(data);
+
+                updatePrices();
+            },
+            'json');
+    }
+
+    function updatePrices(){
+        let total = 0;
+
+        $('.cart-item').each(function(){
+            const tr = $(this);
+            const td_price = tr.find('product_price');
+            const td_sub = tr.find('.sub-total');
+
+            const price = +td_price.attr('data-val');
+            const qty = +tr.find('qty').val();
+
+            td_price.html('$ ' + dollorCommas(price));
+            td_sub.html('$ ' + dollorCommas(price) * qty);
+            total += price * qty;
+
+        });
+        $('#total-price').html('$ ' + dollorCommas(total));
+    }
+    updatePrices();
+</script>
 <?php
 include __DIR__ . '/parts/html-foot.php'; ?>
