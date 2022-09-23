0f9d5991af9cf421e6d4e86fc9ba46484eb4f70b
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-23 23:08:09 +0800

    add empty edit page

---
+++ 01-edit-order-history.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/parts/admin-required.php';
+require __DIR__ . '/parts/connect_db.php';
+?>
+<?php
+include __DIR__ . '/parts/html-head.php'; ?>
+<?php
+include __DIR__ . '/parts/nav-bar-admin.php'; ?>
+
+<?php
+include __DIR__ . '/parts/scripts.php'; ?>
+<?php
+include __DIR__ . '/parts/html-foot.php'; ?>
