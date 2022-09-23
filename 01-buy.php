0b70b1f4b1e20e0eedc6a970f00b9ec827f5342f
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-23 10:27:35 +0800

    add to o&od

---
+++ 01-buy.php
@@ -0,0 +1,40 @@
+<?php
+require __DIR__ . '/parts/connect_db.php';
+
+if (empty($_SESSION['user']) or empty($_SESSION['cart'])) {
+    header('Location: product-list');
+    exit;
+}
+
+$total = 0;
+foreach ($_SESSION['cart'] as $k => $v) {
+    $total += $v['price'] * $v['qty'];
+}
+
+$o_sql = sprintf("INSERT INTO `order-history`(`created_at`, `total`, `member_sid`) VALUES (NOW(),? ,?",);
+
+$stmt = $pdo->query($o_sql);
+
+echo json_encode([
+    'rowCount' => $stmt->rowCount(),
+    'lastInsertId' => $pdo->lastInsertId(),
+]);
+
+$od_sql = sprintf("INSERT INTO `order-details`(`order_sid`, `created_at`, `product_sid`, `product_name`, `quantity`, `total_price`) VALUES (?, NOW(), ?, ?, ?, ?");
+$stmt = $pdo->prepare($od_sql);
+
+foreach ($_SESSION['cart'] as $k => $v) {
+    $stmt->execute([
+        $order_sid,
+        $v['sid'],
+        $v['price'],
+        $v['qty'],
+    ]);
+}
+
+unset($_SESSION['cart']);
+
+?>
+
+<?php include __DIR__ . '/parts/scripts.php'; ?>
+<?php include __DIR__ . '/parts/html-foot.php'; ?>
