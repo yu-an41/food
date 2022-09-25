853fef92d0317f110d7a24f1ee25ad5fa819bee1
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-09-25 11:15:19 +0800

    add space

--- 01-delete-order-history.php
+++ 01-delete-order-history.php
@@ -1,16 +1,16 @@
-<?php
+<?php 
 require __DIR__ . '/parts/admin-required.php';
-require __DIR__ . '/parts/connect_db.php';
+require __DIR__ . '/parts/connect_db.php'; 
 
-$order_num = isset($_GET['order_num']) ? intval($_GET['order_num']) : 0;
+$order_num = isset($_GET['order_num'])? intval($_GET['order_num']): 0;
 
 $sql = "DELETE FROM `order_history` WHERE `order_num` = ${order_num}";
 
-$pdo->query($sql);
+$pdo ->query($sql);
 
 $come_from = '01-order-history-admin.php';
 
-if (!empty($_SERVER['HTTP_REFERER'])) {
+if(! empty($_SERVER['HTTP_REFERER'])) {
     $come_from = $_SERVER['HTTP_REFERE'];
 }
 header("Location: {$come_from}");
